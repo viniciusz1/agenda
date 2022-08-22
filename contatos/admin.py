850fba7e7ab75dfcd1d20234cd2f6471b4e2d956
Vinícius Bonatti Benner <vinicius101313@gmail.com>
2022-08-21 23:07:16 -0300

    angular é o caralho

--- contatos/admin.py
+++ contatos/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Categoria, Contato
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('id','nome','sobrenome','telefone','email','data_criacao','categoria')
+    list_display_links = ('id','nome','sobrenome')
+    list_filter = ('nome','sobrenome')
+    list_per_page = 10
+admin.site.register(Categoria)
+admin.site.register(Contato, ContatoAdmin)
